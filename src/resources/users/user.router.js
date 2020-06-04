fb5d5859321415e18c48f0e30a12d2e639c61b2c
Jonas Allen <Hello@JonasAllen.Codes>
2020-06-04 02:38:31 -0700

    Completed "12. 4.3 CRUD: Read" | Users: Created get users controller and route. Also updated routs for users get and post request routes.

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,8 +2,15 @@ import express from 'express'
 import userController from './user.controller'
 export const userRouter = express.Router()
 
-userRouter.get('/', (req, res) => res.send('User list'))
+userRouter
+  .route('/')
+  .get(userController.getUsers)
+  .post(userController.createUser)
+
+/*
+userRouter.get('/', userController.getUsers)
 userRouter.post('/', userController.createUser)
+*/
 
 userRouter.get('/:id', (req, res) => {
   res.send(`User id is: ${req.params.id}`)

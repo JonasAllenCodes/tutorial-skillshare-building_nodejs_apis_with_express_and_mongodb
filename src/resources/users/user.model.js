613f3cd0573d89881f98aaab77ddc552953dbad2
Jonas Allen <Hello@JonasAllen.Codes>
2020-06-05 02:38:03 -0700

    Lint: Ran "npm run lint"

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -55,16 +55,10 @@ userSchema.methods.toJSON = function () {
 /** export model */
 export const User = mongoose.model('user', userSchema)
 
-export  function validateUser(data) {
+export function validateUser(data) {
   const schema = Joi.object().keys({
-    email: Joi.string()
-      .required()
-      .email()
-      .label('Not a valid email'),
-    password: Joi.string()
-      .required()
-      .min(6)
-      .label('Password is too short')
+    email: Joi.string().required().email().label('Not a valid email'),
+    password: Joi.string().required().min(6).label('Password is too short'),
   })
   return Joi.validate(data, schema)
 }

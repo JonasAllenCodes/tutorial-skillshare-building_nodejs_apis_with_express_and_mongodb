33cf7400350e45679700eac64a451e95e18aa8e1
Jonas Allen <Hello@JonasAllen.Codes>
2020-06-06 02:11:00 -0700

    Completed 19. 4.10 Model Sanitization" | Security: Model Sanitization (Need to fix deprecations in the console log)

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { sanitizeBody } from 'express-validator'
 import userController from './user.controller'
 export const userRouter = express.Router()
 
@@ -12,5 +13,10 @@ userRouter.post('/', userController.createUser)
 userRouter
   .route('/:id')
   .get(userController.getProfile)
-  .put(userController.updateUser)
+  .put([
+      sanitizeBody('email').trim().escape(),
+      sanitizeBody('username').trim().escape(),
+      sanitizeBody('bio').trim().escape(),
+      sanitizeBody('url').trim().escape(),
+    ], userController.updateUser)
   .delete(userController.deleteUser)
